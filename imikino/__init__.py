57811f45efa3ef3ee7364a3c1a4fb6ed87fc7793
leoFagundes <leofagundes2015@gmail.com>
2023-12-01 16:52:13 -0300

    update all

--- imikino/__init__.py
+++ imikino/__init__.py
@@ -2,13 +2,20 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy  # -> para criar o banco de dados
 from flask_bcrypt import Bcrypt  # -> para criptografar a senha do usuário
 from flask_login import LoginManager  # -> para fazer login no site
+import os
 
 
 app = Flask(__name__)
 app.app_context().push()
 
 app.config['SECRET_KEY'] = '4df2fe84ce1de11eff62613706100034'  # token gerado aleatoriamente pelo secrets para proteger o formulário
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///imikino.db'  # criar o arquivo do banco de dados (o sqlite serve para ele criar o banco de dados no mesmo local do arquivo main)
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///imikino.db'  # criar o arquivo do banco de dados (o sqlite serve para ele criar o banco de dados no mesmo local do arquivo main)
+
+if os.getenv("DATABASE_URL"):
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///imikino.db'  # criar o arquivo do banco de dados (o sqlite serve para ele criar o banco de dados no mesmo local do arquivo main)
+
 
 database = SQLAlchemy(app)
 bcrypt = Bcrypt(app)  # auando fazemos isso só o nosso site é capaz de entender a criptografia da senha, ninugém de fora consegue pegar (foi aplicado no routes)
